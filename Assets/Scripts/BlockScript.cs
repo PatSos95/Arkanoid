120574dc7161c2de2984c9c202bd6b17438ae1c4
PatSos95 <patsos95@gmail.com>
2018-10-11 18:47:04 +0200

    Next level

--- Assets/Scripts/BlockScript.cs
+++ Assets/Scripts/BlockScript.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class BlockScript : MonoBehaviour {
 
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(gameObject);
+
     }
+    /*void ChangeSizeOfRacket (Racket x)
+    {
+        
+    }*/
 }
 
--- Assets/Scripts/Racket.cs
+++ Assets/Scripts/Racket.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Racket : MonoBehaviour {
 
-
     public float speed = 150;
-  
+
+   // Vector2 a = GetComponent<BoxCollider2D>().size;
+
+    
+    
+
     private void FixedUpdate()
     {
         // Get Horizontal Input
